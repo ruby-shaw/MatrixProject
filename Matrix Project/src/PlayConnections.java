dbbd545b725594709f196e1bd7b8a6aeaa04e67f
ruby.shaw <ruby.shaw@MU-C1-17.mullen.pvt.k12.co.us>
2024-09-27 12:22:06 -0600

    Initial Commit

---
+++ Matrix Project/src/PlayConnections.java
@@ -0,0 +1,14 @@
+
+public class PlayConnections
+	{
+
+		public static void main(String[] args)
+			{
+				// create Board -> 4x4 Matrix
+				// fillBoard - two + separate boards
+				//has to have shuffle and deselect function
+				// user selects space by letter and number
+
+			}
+
+	}
